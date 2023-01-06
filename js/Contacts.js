29beb4d85734f54f0d15a4727d75a1ce99da8e3b
Kajalwankhede <kajuwankhede1510@gmail.com>
2023-01-06 16:59:24 +0530

    Ability to perform validation addressbook data and also added event listner to fields

---
+++ js/Contacts.js
@@ -0,0 +1,58 @@
+class Contacts{
+
+    get id() {return this._id;}
+    set id(id){
+        this._id=id;
+    }
+    
+    get name()
+    { 
+          return this._name; 
+      }
+  set name(name) 
+      {
+        const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+         if (nameRegex.test(name)) {
+            this._name = name;
+            }
+              else throw 'Name is incorrect!';
+          }
+
+    get phone() {return this._phone;}
+    set phone(phone){
+        let phoneRegex=RegExp('^[0-9]{10}$');
+        if (phoneRegex.test(phone)) this._phone = phone;
+        else throw 'Phone Number is Invalid';
+    }
+
+    get address() {return this._address;}
+    set address(address){
+        if (checkAddressValid(address)) this._address = address;
+        else throw 'Address is Invalid';
+    }
+
+    get city() {return this._city;}
+    set city(city){
+        this._city = city;
+    }
+
+    get state() {return this._state;}
+    set state(state){
+        this._states = state;
+    }
+
+    get zip() {return this._zip;}
+    set zip(zip){
+        this._zip = zip;
+    }
+}
+
+function checkAddressValid(address){
+    if (!address.includes(" ")) return false;
+    let addressArr=address.split(" ");
+    for(var i=0;i<addressArr.length;i++){
+        if (addressArr[i].length<3) return false;
+    }
+    return true;
+}
+
